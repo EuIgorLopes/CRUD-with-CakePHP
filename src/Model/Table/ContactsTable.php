122c02d207498f5ef770864b36cb2de8158e7c7b
Igor Lopes <igorgrl@gmail.com>
2022-06-02 10:06:37 -0300

    Timestamp refactor

--- src/Model/Table/ContactsTable.php
+++ src/Model/Table/ContactsTable.php
@@ -8,6 +8,7 @@ class ContactsTable extends Table
 {
     public function initialize(array $config): void
     {
+        $this->addBehavior('Timestamp');
         $this->setTable("contacts");
     }
 }
